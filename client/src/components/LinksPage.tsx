7ac3b6504083285d0c9625942c7dc636c2f9c5f8
Nicholas Tan <163990283+n1cholastan@users.noreply.github.com>
2025-04-28 22:50:01 +1200

    style: changed socials style

--- client/src/components/LinksPage.tsx
+++ client/src/components/LinksPage.tsx
@@ -2,6 +2,7 @@ import sal from "../assets/sal.png"
 import LinkButton from "./LinkButton";
 import { LinkProps } from "../utils/FrontendTypes";
 import { useState, useEffect } from "react";
+import { SiInstagram, SiTiktok, SiGmail, SiDiscord} from 'react-icons/si';
 
 
 export default function LinksPage() {
@@ -37,7 +38,33 @@ export default function LinksPage() {
             <LinkButton key={link.id} link={link.link} text={link.text} />
         ))}
       </div>
-    </div>
+      <div className="my-6 flex flex-row w-fit justify-center self-center items-center gap-10">
+            <a
+              href="https://www.instagram.com/uoadessertsociety/"
+              className="bg-red rounded-full w-12 h-12 flex items-center justify-center hover:bg-brown transition-colors duration-300 ease-in-out"
+            >
+              <SiInstagram className="w-6 h-6  fill-cream" />
+            </a>
+            <a
+              href="https://www.tiktok.com/@uoadessertsociety"
+              className="bg-red rounded-full w-12 h-12 flex items-center justify-center hover:bg-brown transition-colors duration-300 ease-in-out"
+            >
+              <SiTiktok className="w-6 h-6  fill-cream" />
+            </a>
+            <a
+              href="mailto:uoadessertsociety@gmail.com"
+              className="bg-red rounded-full w-12 h-12 flex items-center justify-center hover:bg-brown transition-colors duration-300 ease-in-out"
+            >
+              <SiGmail className="w-6 h-6  fill-cream" />
+            </a>
+            <a
+              href="https://discord.gg/zEQqXeb9QM"
+              className="bg-red rounded-full w-12 h-12 flex items-center justify-center hover:bg-brown transition-colors duration-300 ease-in-out"
+            >
+              <SiDiscord className="w-6 h-6  fill-cream" />
+            </a>
+          </div>
+        </div>
     </div>
   );
 }
