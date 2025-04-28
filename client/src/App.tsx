7af6b39b85db1882cc50dd8e0cbd2786a0e467a3
Nicholas Tan <163990283+n1cholastan@users.noreply.github.com>
2025-04-28 22:49:38 +1200

    fix: routing issues

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,19 @@
-import LinksPage from './components/LinksPage'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router';
+import LinksPage from './components/LinksPage';
+import './App.css';
 
 function App() {
-
-
   return (
-    <>
+    <Router>
       <div className="w-screen">
-        <LinksPage />
+        <Routes>
+          <Route path="/" element={<LinksPage />} />
+          
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
       </div>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
